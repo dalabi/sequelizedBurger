83a7c62823a52143ae036fe1c1e0da285aba77f3
Damola <d.labi@yahoo.com>
2016-08-20 08:35:45 -0400

    first one

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Little Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Medium Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Burger', FALSE);
---
+++ models/burger.js
@@ -0,0 +1,20 @@
+var connection = require ('../config/orm.js');
+
+var burger = {
+	selectAll: function () {
+		
+		});
+	}, {
+
+	
+	insertOne: function () {
+		
+		});
+	}, {
+	updateOne: function () {
+		
+		});
+	}
+};
+
+module.exports = burger;
